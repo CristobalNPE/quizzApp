4a95d35ab7b8d8f23d8d2becf6e82f8b8c560e1a
CristobalNPE <104582650+CristobalNPE@users.noreply.github.com>
2022-08-28 22:01:24 -0400

    add animation for welcomeBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {CSSTransition} from "react-transition-group";
 import Welcome from "./components/Welcome.jsx";
 
 const App = () => {
+    const [isWelcome, setIsWelcome] = React.useState(true)
+
+    function handleStartButton() {
+        setIsWelcome(false)
+    }
+
+
     return (
         <main>
-            <Welcome/>
+            <CSSTransition in={isWelcome} timeout={300} classNames="fadeout" unmountOnExit>
+                <Welcome isWelcome={isWelcome} handleStartButton={handleStartButton}/>
+            </CSSTransition>
+
+            <Quiz/>
+
         </main>
     );
 };
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Welcome = () => {
+const Welcome = (props) => {
     return (
+
         <section className={`welcome__panel`}>
             <h1 className={`welcome__title`}>¿ Lo sabes ?</h1>
             <h2 className={`welcome__sub`}>Pon a prueba tu conocimiento general</h2>
-            <button className={`btn btn__ok`}>Empezar</button>
+            <button className={`btn btn__ok`} onClick={props.handleStartButton}>Empezar</button>
         </section>
     );
 };
