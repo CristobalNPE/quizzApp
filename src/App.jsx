29cc015a677d849deeec8e7d70df1d6b40d24dec
CristobalNPE <104582650+CristobalNPE@users.noreply.github.com>
2022-08-28 20:39:53 -0400

    First commit, proyect creation and cleanup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from "./components/Welcome.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <Welcome/>
+        </main>
+    );
+};
+
+export default App;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+       <section className={`welcome-panel`}>
+           <h1>Lo sabes?</h1>
+           <h2>Pon a prueba tu conocimiento general</h2>
+           <button>Empezar</button>
+       </section>
+    );
+};
+
+export default Welcome;
