4836dfc727f97a661effa66281da12047b1a725c
CristobalNPE <104582650+CristobalNPE@users.noreply.github.com>
2022-08-28 21:18:48 -0400

    Add styles

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Welcome = () => {
     return (
-       <section className={`welcome__panel`}>
-           <h1 className={`welcome__title`}>Lo sabes?</h1>
-           <h2 className={`welcome__sub`}>Pon a prueba tu conocimiento general</h2>
-           <button className={`btn btn__ok`}>Empezar</button>
-       </section>
+        <section className={`welcome__panel`}>
+            <h1 className={`welcome__title`}>¿ Lo sabes ?</h1>
+            <h2 className={`welcome__sub`}>Pon a prueba tu conocimiento general</h2>
+            <button className={`btn btn__ok`}>Empezar</button>
+        </section>
     );
 };
 
